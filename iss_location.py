e86e8d5c342bbf858b94e0b6bc6bc9a425909a2d
Ieva Hermane <ievah@s1vsk.lv>
2025-10-01 17:13:35 +0300

    ar API

---
+++ iss_location.py
@@ -0,0 +1,11 @@
+import requests
+
+atbilde = requests.get(url="http://api.open-notify.org/iss-now.json")
+
+if atbilde.status_code != 200:
+    raise Exception("Nav derīgas atbildes no API.")
+
+dati = atbilde.json()["iss_position"]
+platums = dati["latitude"]
+garums = dati["longitude"]
+print(platums,garums)
